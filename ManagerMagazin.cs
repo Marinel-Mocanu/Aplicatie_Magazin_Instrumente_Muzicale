676382f8f8d3da848706946a4fb449f6dfe1cc38
Marinel-Mocanu <marinel.mocanu@student.usv.ro>
2026-03-17 14:11:26 +0200

    Add project files.

---
+++ ManagerMagazin.cs
@@ -0,0 +1,57 @@
+﻿namespace Aplicatie_Magazin_Instrumente_Muzicale
+{
+        public class Instrument
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public string Brand { get; set; }
+            public Instrument_Category Category { get; set; }
+            public double Price { get; set; }
+            public string Description { get; set; }
+            public int Quantity { get; set; }
+            public double Discount { get; set; }
+            public double Final_Price()
+            {
+                return Price - (Price - Discount / 100);
+            }
+            public Instrument()
+            {
+                ID = 0;
+                Name = string.Empty;
+                Brand = string.Empty;
+                Category = new Instrument_Category();
+                Price = 0;
+                Description = string.Empty;
+                Quantity = 0;
+                Discount = 0;
+            }
+            public Instrument(string NameIn, string BrandIn, double PriceIn, int idIn, double DiscountIn,int QuantityIn)
+            {
+                ID = idIn;
+                Name = NameIn;
+                Brand = BrandIn;
+                Price = PriceIn;
+                Discount = DiscountIn;
+                Quantity=QuantityIn;
+            }
+            public enum Instrument_Category
+            {
+                Guitars,
+                Drums,
+                Keyboards,
+                Mics,
+                Amps,
+                Synths,
+                Cables,
+                Strings,
+                Accessories
+            }
+        public string Info()
+        {
+            string info = $"ID:{ID} Name:{Name} Brand:{Brand} Price:{Price} Quantity:{Quantity}";
+            return info;
+        }
+            
+        }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,163 @@
+﻿using Aplicatie_Magazin_Instrumente_Muzicale;
+namespace Magazin
+{
+    class Program
+    {
+        public static void Main()
+        {
+            List<Instrument> instruments = new List<Instrument>();
+            Instrument? instrumentNou = null;
+            List<Client> clients = new List<Client>();
+            Client? clientNou = null;
+            string optiune;
+
+            do
+            {
+                Console.WriteLine("INSTRUMENTE");
+                Console.WriteLine("C. Citire informatii instrument:");
+                Console.WriteLine("I. Afisarea informatii instrument");
+                Console.WriteLine("A. Afisare instrumentele din lista");
+                Console.WriteLine("S. Salvare instrument in lista");
+                Console.WriteLine("CLIENTI");
+                Console.WriteLine("C1.Citire info client");
+                Console.WriteLine("S1.Salvare info client");
+                Console.WriteLine("I1.Info client");
+                Console.WriteLine("A1. Afisare clientii din lista");
+                Console.WriteLine("P. Afisare parola");
+
+                Console.WriteLine("X. Inchidere program");
+
+                Console.WriteLine("Alegeti o optiune");
+                optiune = Console.ReadLine()?.ToUpper() ?? string.Empty;
+
+                switch (optiune)
+                {
+                    case "C":
+                        instrumentNou = CitireInstrumentTastatura();
+                        break;
+                    case "C1":
+                        clientNou = CitireClientTastatura();
+                        break;
+
+                    case "I":
+                        AfisareInstrument(instrumentNou);
+                        break;
+                    case "I1":
+                        AfisareClient(clientNou);
+                        break;
+                    case "A":
+                        AfisareInstrumente(instruments);
+                        break;
+                    case "A1":
+                        AfisareClienti(clients);
+                        break;
+                    case "S":
+                        instrumentNou.ID = instruments.Count + 1;
+                        instruments.Add(instrumentNou);
+                        Console.WriteLine("Instrument salvat");
+                        break;
+                    case "S1":
+                        clientNou.ID = clients.Count + 1;
+                        clients.Add(clientNou);
+                        Console.WriteLine("Client salvat");
+                        break;
+                    case "P":
+                        Console.WriteLine("Introduceti username-ul:");
+                        string nume = Console.ReadLine();
+                        Console.WriteLine("Introduceti email-ul:");
+                        string email = Console.ReadLine();
+                        string parolaGasita = GetPassword(clients, email, nume);
+                        if(parolaGasita != null)
+                        {
+                            Console.WriteLine($"Parola: {parolaGasita}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nu a fost gasit contul");
+                        }
+                        break;
+
+                    case "X":
+                        Console.WriteLine("Close");
+                        return;
+                    default:
+                        Console.WriteLine("Optiune inexistenta");
+                        break;
+
+                }
+
+            } while (optiune.ToUpper() != "X");
+            Console.ReadKey();
+        }
+        public static Instrument CitireInstrumentTastatura()
+        {
+            Console.WriteLine("Introduceti numele:");
+            string nume=Console.ReadLine();
+            Console.WriteLine("Introduceti brandul:");
+            string brand=Console.ReadLine();
+            Console.WriteLine("Introduceti price:");
+            double.TryParse(Console.ReadLine(), out double price);
+
+            Console.WriteLine("Introduceti discount:");
+            double.TryParse(Console.ReadLine(), out double discount);
+            Console.WriteLine("Introduceti cantitate:");
+            int.TryParse(Console.ReadLine(), out int cantitate);
+
+            Instrument instrument = new Instrument(nume, brand, price, 0, discount,cantitate);
+            return instrument;
+           
+        }
+        public static Client CitireClientTastatura()
+        {
+            Console.WriteLine("Introduceti username-ul:");
+            string nume = Console.ReadLine();
+            Console.WriteLine("Introduceti email-ul:");
+            string email = Console.ReadLine();
+            Client client = new Client(0, nume, email);
+            Console.WriteLine("Introduceti o parola:");
+            string parola=Console.ReadLine();
+            client.SetPassword(parola);
+            return client;
+
+
+        }
+        public static void AfisareInstrument(Instrument instrument)
+        {
+            Console.WriteLine(instrument.Info());
+        }
+        public static void AfisareClient(Client client)
+        {
+            Console.WriteLine(client.Info());
+        }
+        public static void AfisareInstrumente(List<Instrument> instrumente)
+        {
+            Console.WriteLine("Instrumentele sunt:");
+            foreach(Instrument instrument in instrumente)
+            {
+                AfisareInstrument(instrument);
+            }
+        }
+        public static void AfisareClienti(List<Client> clienti)
+        {
+            Console.WriteLine("Clientii sunt:");
+            foreach (Client client in clienti)
+            {
+                AfisareClient(client);
+            }
+        }
+        public static string GetPassword(List<Client> clients,string email,string nume)
+        {
+            foreach (Client client in clients)
+            {
+                if(client.Name.ToLower()==nume.ToLower() && client.Email.ToLower() == email.ToLower())
+                {
+                    return client.GetPass();
+                }
+            }
+            return null;
+        }
+
+        
+    }
+}
+
